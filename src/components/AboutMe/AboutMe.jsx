776ab9d3d77f9dcffde4ccc5f5b2e18760e5be42
BadStandUp <sackboysema@yandex.ru>
2023-07-05 21:56:30 +0500

    feat: vite migration

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
 				<div className='about-me__description'>
 					<h3 className='about-me__name'>Семён</h3>
 					<p className='about-me__about'>Фронтенд-разработчик, 23 года</p>
-					<p className='about-me__paragraph'>С другой стороны постоянный количественный рост и сфера нашей активности требуют определения и уточнения направлений прогрессивного развития. Не следует, однако забывать, что дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание соответствующий условий активизации.</p>
+					<p className='about-me__paragraph'>Я – фронтендерский клоун, играющий со светом и тенями в виртуальном цирке. Мой браузер – мой верный напарник, открывающий двери в мир красоты и интерактивности. Я мастерски воссоздаю веб-приключения, где каждая линия кода – это пироэффект, каждая анимация – акробатическое представление.</p>
 					<a href="https://github.com/BadStandUp" className='about-me__link' rel='nofollow noopener noreferrer' target='_blank'>Github</a>
 				</div>
 				<img src={author} alt="Фото" className='about-me__image'/>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -18,17 +18,12 @@ export default function Navigation({ loggedIn }) {
 			<div className='navigation__container'>
 				{!loggedIn && <div className='navigation__auth'>
 					<NavLink to='/signup' className='navigation__link navigation__link_auth '>Регистрация</NavLink>
-					<NavLink to='/signin'
-							 className='navigation__link navigation__link_auth navigation__link_button'>Войти</NavLink>
+					<NavLink to='/signin' className='navigation__link navigation__link_auth navigation__link_button'>Войти</NavLink>
 				</div>}
 				{loggedIn && <div className='navigation__bar'>
-					<NavLink to='/movies'
-							 className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Фильмы</NavLink>
-					<NavLink to='/saved-movies'
-							 className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Сохранённые
-						фильмы</NavLink>
-					<NavLink to='/profile'
-							 className={({isActive}) => `navigation__link-account ${isActive ? "navigation__link-account_active" : ""}`}>
+					<NavLink to='/movies' className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Фильмы</NavLink>
+					<NavLink to='/saved-movies' className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Сохранённые фильмы</NavLink>
+					<NavLink to='/profile' className={({isActive}) => `navigation__link-account ${isActive ? "navigation__link-account_active" : ""}`}>
 						<p className='navigation__account-text'>Аккаунт</p>
 						<img src={account_icon} alt="Аккаунт" className='navigation__account-icon'/>
 					</NavLink>
@@ -39,23 +34,15 @@ export default function Navigation({ loggedIn }) {
 				<div className="mobile-nav__container">
 					<button className='mobile-nav__quit-button' onClick={menuToggle}></button>
 					{!loggedIn && <div className='mobile-nav__auth'>
-						<NavLink to='/' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Главная</NavLink>
-						<NavLink to='/signup' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Регистрация</NavLink>
-						<NavLink to='/signin' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Войти</NavLink>
+						<NavLink to='/' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Главная</NavLink>
+						<NavLink to='/signup' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Регистрация</NavLink>
+						<NavLink to='/signin' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Войти</NavLink>
 					</div>}
 					{loggedIn && <div className='mobile-nav__bar'>
-						<NavLink to='/' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Главная</NavLink>
-						<NavLink to='/movies' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Фильмы</NavLink>
-						<NavLink to='/saved-movies' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Сохранённые
-							фильмы</NavLink>
-						<NavLink to='/profile' onClick={linkClick}
-								 className={({isActive}) => `mobile-nav__link-account ${isActive ? "mobile-nav__link-account_active" : ""}`}>
+						<NavLink to='/' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Главная</NavLink>
+						<NavLink to='/movies' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Фильмы</NavLink>
+						<NavLink to='/saved-movies' onClick={linkClick} className={({isActive}) => `mobile-nav__link ${isActive ? "mobile-nav__link_active" : ""}`}>Сохранённые фильмы</NavLink>
+						<NavLink to='/profile' onClick={linkClick} className={({isActive}) => `mobile-nav__link-account ${isActive ? "mobile-nav__link-account_active" : ""}`}>
 							<p className='mobile-nav__account-text'>Аккаунт</p>
 							<img src={account_icon} alt="Аккаунт" className='mobile-nav__account-icon'/>
 						</NavLink>
